2d2ee3b34024cb9d5623ebed9f1b778788c41c55
AbdillahNS <abdillahnoersaid@gmail.com>
2025-02-26 21:07:50 +0700

    Praktikum 6 Jobsheet 3

---
+++ app/Models/UserModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_user'; //Mendefinikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id'; // Mendefinikan primary key tabel ini
+}
