15ac9f19eee7975aa242dc0aaeaca6255e056207
AbdillahNS <abdillahnoersaid@gmail.com>
2025-03-18 20:31:46 +0700

    Praktikum 6

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -20,6 +20,6 @@ class UserModel extends Model
 
     public function level(): BelongsTo
     {
-        return $this->belongsTo(LevelModel::class, 'level_id', );
+        return $this->belongsTo(LevelModel::class, 'level_id');
     }
 }
